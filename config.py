22d76b1e3c9cdefed1e44a54e7896218cceea42c
Hua Yanghao <huayanghao@gmail.com>
2012-10-23 17:23:40 +0800

    the first mako template is working

--- config.py
+++ config.py
@@ -1,4 +1,6 @@
+import os
 import memcache
 
 mc = memcache.Client(['127.0.0.1:11211'], debug=0)
-
+static_root = '/home/hua/static'
+template_root = '/home/hua/xiaoyezi.org/template'
--- view/static.py
+++ view/static.py
@@ -1,14 +1,20 @@
+import os
 from webapp2 import RequestHandler, Response
+from mako.template import Template
 
 from view.cache import CacheHandler
 from config import mc
+from config import static_root
+from config import template_root
 
 class StaticFolder(CacheHandler):
     def __init__(self, request=None, response=None):
         CacheHandler.__init__(self, request, response, mc)
 
-    def fetch_get_request(self, folder='', filename=''):
-        return 'folder: %s, filename: %s, path: %s' % (folder, filename, self.request.path_info)
+    def fetch_get_request(self, folder=''):
+        t = Template(filename=os.path.join(template_root, 'base.html'))
+        req_uri = self.request.path_info
+        return t.render(req_uri=req_uri)
 
 class StaticMarkdown(CacheHandler):
     def __init__(self, request=None, response=None):
