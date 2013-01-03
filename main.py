8a8b058946d7482be3ecf97d4dcd4f71bc2b4d5c
Hua Yanghao <huayanghao@gmail.com>
2013-01-03 13:54:47 +0800

    view: using logger instead of fd.write

--- main.py
+++ main.py
@@ -1,10 +1,11 @@
 import webapp2
+import logging
 
 from view.static import StaticBlog
 
 urls = [ (r'/blog/?([a-z]*)/?([A-Za-z0-9-]*)/?(.*)/?', StaticBlog),
 ]
 
-
+logging.basicConfig(level=logging.DEBUG)
 
 application = webapp2.WSGIApplication(urls, debug=True)
--- view/cache.py
+++ view/cache.py
@@ -1,17 +1,18 @@
 from webapp2 import RequestHandler, Response
+import logging
 
 class CacheHandler(RequestHandler):
     def __init__(self, request=None, response=None, mc=None, ignore_trailing_slash=True):
         RequestHandler.__init__(self, request, response)
         self.ignore_trailing_slash = ignore_trailing_slash
+        self.log = logging.getLogger(self.__class__.__name__)
         if mc == None:
             raise "No Memcache Client provided"
         else:
             self.mc = mc
-            self.fd = open('/home/hua/xiaoyezi.log', 'a')
 
     def __del__(self):
-        self.fd.close()
+        pass
 
     def get(self, *args, **kargs):
         req_uri = self.request.path_info.encode('UTF-8')
@@ -19,9 +20,9 @@ class CacheHandler(RequestHandler):
             req_uri = req_uri[:-1]
         value = self.mc.get(req_uri)
         if value != None: # cache hit
-            self.fd.write("%s HIT\n" % req_uri)
+            self.log.info("Cache hit : %s" % req_uri)
         else: # cache miss
-            self.fd.write("%s MISS\n" % req_uri)
+            self.log.info("Cache miss : %s" % req_uri)
             value = self.fetch_get_request(*args, **kargs)
             self.mc.set(req_uri, value)
         return Response(value)
